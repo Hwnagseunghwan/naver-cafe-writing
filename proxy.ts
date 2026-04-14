432be8aef6f98e23b599f575f7532cb3619d85e5
Hwnagseunghwan <zen896@gmail.com>
2026-04-14 17:32:50 +0900

    proxy.ts export 수정, middleware.ts 제거

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 
-export const { auth: proxy } = NextAuth(authConfig);
+const { auth } = NextAuth(authConfig);
+export { auth as proxy };
 
 export const config = {
   matcher: ["/((?!login|api|_next/static|_next/image|favicon.ico|uploads).*)"],
